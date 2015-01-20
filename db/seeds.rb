b08b7c289c24bcb42862512c838ad63fbd49ca14
Peter Soderberg <psoderberg@gmail.com>
2015-01-20 17:19:54 -0600

    heroku case sensitivities

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'CSV'
+require 'csv'
 require 'date'
 require 'zlib'
 # Crime.delete_all
